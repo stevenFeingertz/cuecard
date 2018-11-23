8622e1ebdf660307196c65a9ee5139b60c7a6334
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-22 20:46:47 -0600

    refactoring code

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// ENV SETUP
+require('./config/config.js');
+
+// IMPORT
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// SETUP EXPRESS
+var app = express();
+
+// HEROKU PORT VAR
+const port = process.env.PORT;
+
+// EXPRESS MIDDLEWARE
+app.use(bodyParser.json());
+
+// START SERVER ON PORT ??
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}.`);
+});
