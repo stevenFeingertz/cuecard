4eed59bb4142f5211b8b05ac275c121268adf176
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-26 13:29:01 -0500

    updated production db with ssl certs

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -5,7 +5,12 @@ var connection = mysql.createConnection({
   user     : process.env.DATABASE_USERNAME,
   password : process.env.DATABASE_PASSWORD,
   database : process.env.DATABASE_NAME,
-  port     : 3306
+  port     : 3306,
+  ssl  : {
+    ca      : fs.readFileSync(__dirname + '/server/db/ssl/cleardb-ca.pem'),
+    cert    : fs.readFileSync(__dirname + '/server/db/ssl/be81114c16f5d7-cert.pem'),
+    key    : fs.readFileSync(__dirname + '/server/db/ssl/cleardb_id-key-no-password.pem')
+  }
 });
 
 module.exports = {
