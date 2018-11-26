209a7a84c61d0fb3d21952a43251ec54bbff89dc
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-26 12:21:11 -0500

    updated config vars for db

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,21 @@
 // ENVIRONMENT VARIABLES
 
-// process.env.NODE_ENV will be set by heroku by default as 'production'
+// process.env.NODE_ENV will be set by heroku by default as 'production'.
+// if we are local, it will set to development
 var env = process.env.NODE_ENV || 'development';
 
+// configure database
 if (env === 'development') {
 	process.env.PORT = 3000;
-	// process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+	process.env.DATABASE_HOST = 'localhost';
+	process.env.DATABASE_USERNAME = 'root';
+	process.env.DATABASE_PASSWORD = '&d7&wXK#QKzB';
+	process.env.DATABASE_NAME = 'cuecard.io';
+}
+
+if (env === 'production') {
+	process.env.DATABASE_HOST = 'us-cdbr-iron-east-01.cleardb.net';
+	process.env.DATABASE_USERNAME = 'be81114c16f5d7';
+	process.env.DATABASE_PASSWORD = '65241429';
+	process.env.DATABASE_NAME = 'heroku_45f49aa72bc6398';
 }
--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '&d7&wXK#QKzB',
-  database : 'cuecard.io',
+  host     : process.env.DATABASE_HOST,
+  user     : process.env.DATABASE_USERNAME,
+  password : process.env.DATABASE_PASSWORD,
+  database : process.env.DATABASE_NAME,
   port     : 3306
 });
 
