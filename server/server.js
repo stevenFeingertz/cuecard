06765a7963489a6d0707f4726b2efdcea5692c72
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-26 13:52:24 -0500

    bug fixes

--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ app.get('/test', (req, res) => {
 
 		res.send(results);
 
+
+
 	  // error will be an Error if one occurred during the query
 	  // results will contain the results of the query
 	  // fields will contain information about the returned results fields (if any)
