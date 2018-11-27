0edf0f20f4d9f3dbf43d2b50daf964b64d601496
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-26 21:15:32 -0500

    db changes

--- server/config/config.js
+++ server/config/config.js
@@ -6,8 +6,8 @@ var env = process.env.NODE_ENV || 'development';
 // production vars in heroku
 if (env === 'development') {
 	process.env.PORT = 3000;
-	// process.env.DATABASE_HOST = 'localhost';
-	// process.env.DATABASE_USERNAME = 'root';
-	// process.env.DATABASE_PASSWORD = '&d7&wXK#QKzB';
-	// process.env.DATABASE_NAME = 'cuecard.io';
+	process.env.DATABASE_HOST = 'localhost';
+	process.env.DATABASE_USERNAME = 'root';
+	process.env.DATABASE_PASSWORD = '&d7&wXK#QKzB';
+	process.env.DATABASE_NAME = 'cuecard.io';
 }
