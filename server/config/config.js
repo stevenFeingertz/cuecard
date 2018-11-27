ba51a632bae6a7585ffceae2e3676cc7e54980c7
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-26 21:44:06 -0500

    remove artifact files

--- server/config/config.js
+++ server/config/config.js
@@ -6,8 +6,8 @@ var env = process.env.NODE_ENV || 'development';
 // production vars in heroku
 if (env === 'development') {
 	process.env.PORT = 3000;
-	process.env.DATABASE_HOST = 'localhost';
-	process.env.DATABASE_USERNAME = 'root';
-	process.env.DATABASE_PASSWORD = '&d7&wXK#QKzB';
-	process.env.DATABASE_NAME = 'cuecard.io';
+	process.env.DATABASE_HOST = 'us-cdbr-iron-east-01.cleardb.net';
+	process.env.DATABASE_USERNAME = 'be81114c16f5d7';
+	process.env.DATABASE_PASSWORD = '65241429';
+	process.env.DATABASE_NAME = 'heroku_45f49aa72bc6398';
 }
--- server/server.js
+++ server/server.js
@@ -15,17 +15,13 @@ app.use(bodyParser.json());
 // ROUTES (TEMP)
 app.get('/test', (req, res) => {
 
-	connection.connect();
-
 	connection.query({
 	  sql: 'SELECT * FROM `account` WHERE `ownerID` = ? and `id` = ?',
 	  timeout: 40000, // 40s
 	  values: [22, 1]
 	}, function (error, results, fields) {
 
-		if (error) {
-			return res.send(error); 
-		}
+		if (error) throw error;
 
 		res.send(results);
 
@@ -33,9 +29,6 @@ app.get('/test', (req, res) => {
 	  // results will contain the results of the query
 	  // fields will contain information about the returned results fields (if any)
 	});
-
-	// close db connection
-	connection.end();
 
 
 
