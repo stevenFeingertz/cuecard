3d8a24a0963cbfcdf41b5dc8cd3e90b7b826ad87
Steven Feingertz <stevenfeingertz@stevens-mbp.attlocal.net>
2018-11-27 20:49:54 -0500

    quick update

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql');
-const fs = require('fs');
+const mysql = require("mysql");
+const fs = require("fs");
 
 var connection = mysql.createConnection({
-  host     : process.env.DATABASE_HOST,
-  user     : process.env.DATABASE_USERNAME,
-  password : process.env.DATABASE_PASSWORD,
-  database : process.env.DATABASE_NAME,
-  port     : 3306
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: 3306
   // ssl      : {
   //       ca      : fs.readFileSync(__dirname + '/ssl/rds-combined-ca-bundle.pem')
   // }
 });
 
 module.exports = {
-    connection: connection
-}
+  connection: connection
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 // ENV VARS
-require('dotenv').config();
+require("dotenv").config();
 
 // IMPORT
-const express = require('express');
-const bodyParser = require('body-parser');
-const {connection} = require('./db/mysql.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { connection } = require("./db/mysql.js");
 
 // SETUP EXPRESS
 var app = express();
@@ -13,50 +13,36 @@ var app = express();
 app.use(bodyParser.json());
 
 // ROUTES (TEMP)
-app.get('/', (req, res) => {
-	res.status(400).send();
+app.get("/", (req, res) => {
+  res.status(400).send();
 });
 
-app.get('/test', (req, res) => {
+app.get("/test", (req, res) => {
+  connection.query(
+    {
+      sql: "SELECT * FROM `account` WHERE `ownerID` = ? and `id` = ?",
+      timeout: 40000, // 40s
+      values: [22, 1]
+    },
+    function(error, results, fields) {
+      if (error) throw error;
 
-	connection.query({
-	  sql: 'SELECT * FROM `account` WHERE `ownerID` = ? and `id` = ?',
-	  timeout: 40000, // 40s
-	  values: [22, 1]
-	}, function (error, results, fields) {
+      res.json(results);
 
-		if (error) throw error;
+      // error will be an Error if one occurred during the query
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
+    }
+  );
 
-		res.json(results);
-
-	  // error will be an Error if one occurred during the query
-	  // results will contain the results of the query
-	  // fields will contain information about the returned results fields (if any)
-	});
-
-
-
-
-
-	// Todo.find().then((todos) => {
-	// 	res.send({todos});
-	// }).catch((e) => {
-	// 	res.status(400).send();
-	// });
-
-
-
-
+  // Todo.find().then((todos) => {
+  // 	res.send({todos});
+  // }).catch((e) => {
+  // 	res.status(400).send();
+  // });
 });
-
-
-
-
-
-
-
 
 // START SERVER
 app.listen(process.env.PORT, () => {
-	console.log(`Server is up on port ${process.env.PORT}.`);
+  console.log(`Server is up on port ${process.env.PORT}.`);
 });
